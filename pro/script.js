159e3fa4cca6eeb53375bbf468ecd9baf78fb49b
shenao <2020802508@qqcom>
2021-12-14 21:15:06 +0800

    建表修改

--- pro/script.js
+++ pro/script.js
@@ -5,16 +5,16 @@
 function Renderboard(numRows,numCols){
     let boardEl = document.querySelector("#board")
 
-    for(let i=0;i<numRows;i++){
+    for (let i=0;i<numRows;i++) {
         let trEl =document.createElement("tr")
-        let cellEl =document.createElement("div")
-        cellEl.className="cell"
 
-        for(let j=0;j<numCols;j++){
+        for (let j=0;j<numCols;j++) {
+            let cellEl =document.createElement("div")
+            cellEl.className="cell"
             let tdEl =document.createElement("td")
             tdEl.append(cellEl)
 
-            trEl.appeend(tdEl)
+            trEl.append(tdEl)
         }
 
         boardEl.append(trEl)
