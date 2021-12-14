77f5d18dc182f01534c9e01e7086c0c94102a3e5
shenao <2020802508@qqcom>
2021-12-14 23:49:27 +0800

    埋雷

--- pro/script.js
+++ pro/script.js
@@ -2,7 +2,7 @@
 
 
 
-function Renderboard(numRows,numCols){
+function Renderboard(numRows,numCols,grid){
     let boardEl = document.querySelector("#board")
 
     for (let i=0;i<numRows;i++) {
@@ -11,15 +11,43 @@ function Renderboard(numRows,numCols){
         for (let j=0;j<numCols;j++) {
             let cellEl =document.createElement("div")
             cellEl.className="cell"
+            cellEl.innerText=grid[i][j];
 
             let tdEl =document.createElement("td")
-            tdEl.append(cellEl)
+            tdEl.append(cellEl);
 
-            trEl.append(tdEl)
+            trEl.append(tdEl);
         }
 
-        boardEl.append(trEl)
+        boardEl.append(trEl);
     }
 }
 
-Renderboard(15,20);
+function Initialize(numRows,numCols,numMines){
+    let grid = new Array(numRows)
+    for (let i=0;i<numRows;i++){
+        grid[i] = new Array(numCols);
+        for (let j=0;j<numCols;j++){
+            grid[i][j]=0;
+        }
+    }
+
+    let mines = [];
+    for (let k=0;k<numMines;k++){
+        let cellMl = Math.trunc(Math.random() * numCols * numRows);
+        let row = Math.trunc(cellMl / numCols);
+        let col = cellMl % numCols
+        console.log(cellMl,row,col);
+
+        grid[row][col]=-1
+        mines.push([row,col]);
+
+
+    }
+    console.log(grid);
+
+    return grid;
+}
+
+let grid = Initialize(15,20,20);
+Renderboard(15,20,grid);
