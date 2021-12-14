5f7bd9152e554ddd03bea24f4aaa99ee0b4453f4
shenao <2020802508@qqcom>
2021-12-15 00:32:24 +0800

    更改4

--- pro/script.js
+++ pro/script.js
@@ -23,6 +23,11 @@ function Renderboard(numRows,numCols,grid){
     }
 }
 
+const directions=[
+    [-1,-1],[-1,0],[-1,1],
+    [0,-1],[0,1],
+    [1,-1],[1,0],[1,1]
+]
 function Initialize(numRows,numCols,numMines){
     let grid = new Array(numRows)
     for (let i=0;i<numRows;i++){
@@ -41,12 +46,29 @@ function Initialize(numRows,numCols,numMines){
 
         grid[row][col]=-1
         mines.push([row,col]);
-
-
     }
-    console.log(grid);
-
-    return grid;
+        //计算有雷的周边为0的周边雷数
+        for (let [row,col] of mines){
+            for (let [drow,dcol] of directions){
+                let cellRow = row+drow;
+                let cellCol = col+dcol;
+                if  (cellRow < 0 || cellCol < 0|| cellRow >= numRows || cellCol >= numCols){
+                    continue;
+                }
+                if (grid[cellRow][cellCol] === 0){
+                    let count = 0
+                    for (let [arow,acol] of directions){
+                        if (grid[cellRow+ arow][cellCol + acol] === -1){
+                            count += 1;
+                        }
+                    }
+                    if (count > 0){
+                        grid[cellRow][cellCol] = count
+                    }
+                }
+            }
+        }
+    return grid;;
 }
 
 let grid = Initialize(15,20,20);
